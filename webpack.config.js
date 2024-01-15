f9fcc523373bce8f4c87478a653d66efa50467fd
Yuriy <ykantakuzin@bk.ru>
2024-01-15 18:17:31 +0300

    добавbk папку public

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
   mode: process.env.NODE_ENV || 'production',
   entry: './src/script.tsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.[contenthash].js',
     publicPath: '/',
   },
